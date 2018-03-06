b9c63929048b86f6c8bc2ebc9aab21ee5048b4b4
mightypanders <mighty.panders@gmail.com>
2018-03-06 21:39:56 +0100

    conventions, modified fullbackup

--- fullbackup.sh
+++ fullbackup.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
+backupfolder="/var/backups/automatic/"
 folders="$@"
+
+if [ ! -d "$backupfolder" ]; then
+	mkdir -p "$backupfolder"
+fi
 
 for f in $folders
 do
 	bakfilename=$(echo "$f" | sed 's,/,_,g')
-	datestring=$(date +%F)
+	datestring=$(date +%F_%H:%M)
 
-	tar -zcpf /var/backups/"$bakfilename"_"$datestring".tgz "$f"
+	tar -zcpf "$backupfolder""$bakfilename"_"$datestring".tgz "$f"
 
 done
---
+++ notes.md
@@ -0,0 +1,18 @@
+### find date
+https://stackoverflow.com/questions/18339307/find-files-in-terminal-between-a-date-range
+
+## Datumskonvention
+datestring => yyyy-MM-dd_hh:mm
+
+## Dateiname
+#### full
+`f_<date>_<folder>`
+#### incremental
+`i_<date>_<folder>`
+#### differential
+`d_<date>_<folder>`
+
+## backup location
+`/var/backups/automatic/`
+## log location
+`/var/log/autobkp.log`
