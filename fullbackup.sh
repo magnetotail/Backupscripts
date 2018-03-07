b0c939b2ce0b48d6452cdef45625bd68be9deaf7
magnetotail <nils.wiechert@web.de>
2018-03-07 17:59:15 +0100

    Skip not existing directories

--- fullbackup.sh
+++ fullbackup.sh
@@ -8,6 +8,11 @@ fi
 
 for f in $folders
 do
+	if [ ! -d "$f" ]; then
+		echo ERROR: "$f" is not a directory or not existing. Skipping...
+		continue
+	fi
+
 	bakfilename=$(echo "$f" | sed 's,/,_,g')
 	datestring=$(date +%F_%H:%M)
 
