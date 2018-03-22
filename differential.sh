6545972895594a4a0a45ef65a47598b38ea455c1
magnetotail <nils.wiechert@web.de>
2018-03-22 09:11:32 +0100

    Implement differential backuping

--- differential.sh
+++ differential.sh
@@ -1,3 +1,41 @@
 #!/bin/bash
 #TODO
 # Skript schreiben
+backupfolder="/var/backups/automatic/"
+argCount=0
+while getopts ":o:" opts; do
+	case ${opts} in
+		o) if [ ! -d "$OPTARG" ]; then
+				echo ERROR: $OPTARG is not a directory or not existing. Exiting..
+				exit
+		fi
+		((argCount++))
+		backupfolder=${OPTARG};;
+
+	esac
+done
+if [[ argCount -gt 0 ]]; then
+	shift "(($argCount * 2))"
+fi
+
+folders="$@"
+
+lastbackupfile=$(ls -Art "$backupfolder" | tail -n 1)
+
+for f in $folders
+do
+	if [ ! -d "$f" ]; then
+		echo ERROR: "$f" is not a directory or not existing. Skipping...
+		continue
+	fi
+
+	find "$f" -type f -newer "$backupfolder""$lastbackupfile" > tmp.txt
+done
+
+datestring=$(date +%F_%H:%M)
+
+bakfilename=$(echo "$f" | sed 's,/,_,g')
+
+tar -zcpf "$backupfolder""d_""$datestring"_"$bakfilename".tgz -T tmp.txt
+
+
