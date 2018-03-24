43df336214be7469ed311e196607c611c7aff5ef
magnetotail <nils.wiechert@web.de>
2018-03-24 14:52:16 +0100

    Fix loop bug
    Loop did not backup multiple folders

--- differential.sh
+++ differential.sh
@@ -21,13 +21,12 @@ while getopts "o:idf" opts; do
 		;;
 	esac
 done
-shift $(($OPTIND-1))
-
 
 length=${#backupfolder}
 last_char=${backupfolder:length-1:1}
 [[ $last_char != "/" ]] && backupfolder="$backupfolder/"; :
 
+shift $(($OPTIND-1))
 folders="$@"
 
 lastbackupfile=$(ls -Art "$backupfolder""$suchmarker"* | tail -n 1)
@@ -40,13 +39,13 @@ do
 	fi
 
 	find "$f" -type f -newer "$lastbackupfile" > tmp.txt
+
+	datestring=$(date +%F_%H:%M)
+	
+	bakfilename=$(echo "$f" | sed 's,/,_,g')
+	
+	tar -zcpf "$backupfolder""$marker"_"$datestring"_"$bakfilename".tgz -T tmp.txt
+	
+	rm tmp.txt
 done
-
-datestring=$(date +%F_%H:%M)
-
-bakfilename=$(echo "$f" | sed 's,/,_,g')
-
-tar -zcpf "$backupfolder""$marker"_"$datestring"_"$bakfilename".tgz -T tmp.txt
-
-rm tmp.txt
 
